41372f9483522cbc538cba4c4ab7532bc408c7fd
Yang-Yu-Yao <wow35396425@gmail.com>
2025-09-01 01:53:10 +0800

    Site updated: 2025-09-01 01:53:10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756658748165";const preCache = ["/images/taichi.png","/images/bannerHina.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756662786811";const preCache = ["/images/taichi.png","/images/bannerHina.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
